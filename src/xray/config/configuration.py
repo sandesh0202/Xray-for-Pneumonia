77d06ebae476f5a58090648bb277ddfb532db210
Sandesh0202 <sandeshpargaonkar7@gmail.com>
2023-07-20 23:02:33 +0530

    prepare callbacks

--- src/xray/config/configuration.py
+++ src/xray/config/configuration.py
@@ -1,6 +1,10 @@
+import os
 from xray.constants import *
 from xray.utils.common import read_yaml, create_directories  
-from xray.entity.config_entity import DataIngestionConfig
+from xray.entity.config_entity import (DataIngestionConfig,
+                                       PrepareBaseModelConfig,
+                                       PrepareCallbackConfig                                           
+                                    )
 
 
 class ConfigurationManager:
@@ -27,3 +31,40 @@ class ConfigurationManager:
         )
         
         return data_ingestion_config
+    
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+        
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir = Path(config.root_dir),
+            base_model_path = Path(config.base_model_path),
+            updated_base_model_path = Path(config.updated_base_model_path),
+            params_image_size=self.params.image_size,
+            params_image_shape=self.params.IMAGE_SHAPE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+        
+        return prepare_base_model_config 
+    
+    
+    
+    def get_prepare_callback_config(self) -> PrepareCallbackConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+        
+        prepare_callback_config = PrepareCallbackConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+        
+        return prepare_callback_config
